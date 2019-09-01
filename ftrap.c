af91967cf98e6ad35b2a22240e4f44cf90e14bb4
snsinfu <snsinfu@gmail.com>
2019-09-02 01:24:42 +0900

    Refactor
    Still the code is too linux-specific.

--- ftrap.c
+++ ftrap.c
@@ -21,31 +21,36 @@
 enum { ftrap_error = 112 };
 
 static void show_usage(void);
+static int  start_ftrap(pid_t pid, int inofd, int sigfd, int *status);
 static void dummy_handler(int sig);
 
 
 int main(int argc, char **argv)
 {
-    int ino = inotify_init1(IN_CLOEXEC);
-    if (ino == -1) {
+    pid_t pid;
+    int inofd;
+    int sigfd;
+
+    inofd = inotify_init1(IN_CLOEXEC);
+    if (inofd == -1) {
         fprintf(stderr, "error: Cannot create inotify instance - %s\n", strerror(errno));
         return ftrap_error;
     }
 
     for (int ch; (ch = getopt(argc, argv, "hf:")) != -1; ) {
         switch (ch) {
-          case 'h':
+        case 'h':
             show_usage();
             return 0;
 
-          case 'f':
-            if (inotify_add_watch(ino, optarg, IN_MODIFY) == -1) {
+        case 'f':
+            if (inotify_add_watch(inofd, optarg, IN_MODIFY) == -1) {
                 fprintf(stderr, "error: Cannot watch file %s - %s\n", optarg, strerror(errno));
                 return ftrap_error;
             }
             break;
 
-          default:
+        default:
             return ftrap_error;
         }
     }
@@ -53,104 +58,56 @@ int main(int argc, char **argv)
     argv += optind;
 
     if (argc == 0) {
-        fprintf(stderr, "error: Command is not specified. See ftrap -h for help.\n");
+        fprintf(stderr, "error: Command is not specified. See ftrap -h for usage.\n");
         return ftrap_error;
     }
 
-    // Need to set a handler to detect SIGCHLD with signalfd.
+    // Looks like a handler needs to be set to detect SIGCHLD with signalfd.
     if (signal(SIGCHLD, dummy_handler) == SIG_ERR) {
-        fprintf(stderr, "error: ");
+        fprintf(stderr, "error: Failed to watch SIGCHLD - %s\n", strerror(errno));
         return ftrap_error;
     }
 
-    // We will wait for child process using poll(2) via this signalfd.
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGCHLD);
-
-    int sig = signalfd(-1, &mask, SFD_CLOEXEC);
-    if (sig == -1) {
-        fprintf(stderr, "error: cannot monitor SIGCHLD - %s\nn", strerror(errno));
+    sigfd = signalfd(-1, &mask, SFD_CLOEXEC);
+    if (sigfd == -1) {
+        fprintf(stderr, "error: Cannot create signalfd - %s\nn", strerror(errno));
         return ftrap_error;
     }
 
-    pid_t pid = fork();
+    pid = fork();
     if (pid == -1) {
-        fprintf(stderr, "error: cannot fork - %s\n", strerror(errno));
+        fprintf(stderr, "error: Cannot fork - %s\n", strerror(errno));
         return ftrap_error;
     }
-
     if (pid == 0) {
         execvp(argv[0], argv);
-        fprintf(stderr, "error: cannot execute the command - %s\n", strerror(errno));
-        exit(ftrap_error);
+        fprintf(stderr, "error: Cannot execute the command - %s\n", strerror(errno));
+        return ftrap_error;
     }
 
-    enum {
-        poll_inotify,
-        poll_signalfd,
-        poll_count
-    };
-    struct pollfd polls[poll_count] = {
-        [poll_inotify] = { .fd = ino, .events = POLLIN },
-        [poll_signalfd] = { .fd = sig, .events = POLLIN }
-    };
-
-    for (;;) {
-        if (poll(polls, poll_count, -1) == -1) {
-            if (errno == EINTR) {
-                continue;
-            }
-            fprintf(stderr, "error: poll failed - %s\n", strerror(errno));
-            return ftrap_error;
-        }
-
-        if (polls[poll_signalfd].revents & POLLIN) {
-            int status = 0;
-            if (waitpid(pid, &status, 0) == -1) {
-                fprintf(stderr, "error: waitpid failed - %s\n", strerror(errno));
-                return ftrap_error;
-            }
-
-            // Exit with the same code as the child.
-            if (WIFEXITED(status)) {
-                return WEXITSTATUS(status);
-            }
-
-            // Die with the same signal as the child.
-            if (WIFSIGNALED(status)) {
-                kill(getpid(), WTERMSIG(status));
-            }
-
-            return ftrap_error;
-        }
-
-        if (polls[poll_inotify].revents & POLLIN) {
-            char buf[sizeof(struct inotify_event) + NAME_MAX + 1];
-
-            if (read(ino, buf, sizeof buf) == -1) {
-                if (errno == EINTR) {
-                    continue;
-                }
-                fprintf(stderr, "error: Cannot read inotify event - %s\n", strerror(errno));
-                return ftrap_error;
-            }
-
-            struct inotify_event *ev = (void *) buf;
-
-            if (ev->mask & IN_MODIFY) {
-                if (kill(pid, SIGHUP) == -1) {
-                    fprintf(stderr, "error: Cannot signal the child - %s\n", strerror(errno));
-                    return ftrap_error;
-                }
-            }
-        }
+    int status;
+    if (start_ftrap(pid, inofd, sigfd, &status) == -1) {
+        return ftrap_error;
     }
 
-    assert(0);
+    // Exit with the same status as that of the child.
+    if (WIFEXITED(status)) {
+        exit(WEXITSTATUS(status));
+    }
+    if (WIFSIGNALED(status)) {
+        kill(getpid(), WTERMSIG(status));
+    }
+
+    return ftrap_error;
 }
 
-// show_usage prints usage information to stderr.
+// Function: show_usage
+//
+// Outputs program usage to stdout.
+//
 void show_usage(void)
 {
     const char *msg =
@@ -164,6 +121,75 @@ void show_usage(void)
     fputs(msg, stdout);
 }
 
+// Function: start_ftrap
+//
+// Sends SIGHUP to the child process on detecting any inotify event.
+//
+// Parameters:
+//    pid    - pid of the child process
+//    inofd  - inotify file descriptor to watch
+//    sigfd  - signalfd file descriptor for SIGCHLD
+//    status - child process' exit status is returned to *status
+//
+// Returns:
+//    -1 on failure. It does not return on success (instead the process exits).
+//
+int start_ftrap(pid_t pid, int inofd, int sigfd, int *status)
+{
+    enum {
+        poll_inotify,
+        poll_signalfd,
+        poll_count
+    };
+    struct pollfd polls[poll_count] = {
+        [poll_inotify ] = { .fd = inofd, .events = POLLIN },
+        [poll_signalfd] = { .fd = sigfd, .events = POLLIN }
+    };
+
+    for (;;) {
+        if (poll(polls, poll_count, -1) == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            fprintf(stderr, "error: Cannot poll - %s\n", strerror(errno));
+            return -1;
+        }
+
+        // Child process is terminated.
+        if (polls[poll_signalfd].revents & POLLIN) {
+            if (waitpid(pid, status, 0) == -1) {
+                fprintf(stderr, "error: Cannot waitpid - %s\n", strerror(errno));
+                return -1;
+            }
+            return 0;
+        }
+
+        // One of the target files is changed.
+        if (polls[poll_inotify].revents & POLLIN) {
+            char buf[sizeof(struct inotify_event) + NAME_MAX + 1];
+
+            while (read(inofd, buf, sizeof buf) == -1) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                fprintf(stderr, "error: Cannot read inotify event - %s\n", strerror(errno));
+                return -1;
+            }
+
+            if (kill(pid, SIGHUP) == -1) {
+                fprintf(stderr, "error: Cannot signal the child - %s\n", strerror(errno));
+                return -1;
+            }
+        }
+    }
+
+    assert(0);
+}
+
+// Function: dummy_handler
+//
+// Dummy signal handler that does nothing.
+//
 void dummy_handler(int sig)
 {
     (void) sig;
